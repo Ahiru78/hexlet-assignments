fb75fbed414f3539ff03642f70ef35701440c008
@hexlet/cli <support@hexlet.io>
2024-11-07 13:11:56 +0300

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/dto/users/UserPage.java
@@ -0,0 +1,16 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+
+import lombok.Getter;
+
+// BEGIN
+@Getter
+public class UserPage {
+    private User user;
+
+    public UserPage(User user) {
+        this.user = user;
+    }
+}
+// END
---
+++ java-web-ru/template/src/main/java/exercise/dto/users/UsersPage.java
@@ -0,0 +1,17 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+import java.util.List;
+
+import lombok.Getter;
+
+// BEGIN
+@Getter
+public class UsersPage {
+    private List<User> users;
+
+    public UsersPage(List<User> users) {
+        this.users = users;
+    }
+}
+// END
